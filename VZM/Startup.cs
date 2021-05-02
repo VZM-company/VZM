e6cc0b29ef0e3256de57fedc2b214eec250b772d
EvgeniyVashinko <e.vashinko@gmail.com>
2021-05-02 13:01:43 +0300

    add db connection

--- VZM/Startup.cs
+++ VZM/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,8 @@ namespace VZM
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSingleton(new SqlConnection(Configuration["DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
