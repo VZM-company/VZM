e1a7a806207f49163e1cee64345b8a3d7c5ea0ad
Tourie <makar.pundis@yandex.ru>
2021-05-03 19:43:16 +0300

    Main page start

--- VZM/ClientApp/src/app/app.module.ts
+++ VZM/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- VZM/ClientApp/src/app/home/home.component.ts
+++ VZM/ClientApp/src/app/home/home.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  items = [{
+    name: "qwer",
+    price: 132,
+    left: 10,
+    discount: 101
+  }, {
+      name: "rewfasdv",
+      price: 121,
+      left: 10,
+      discount: 1401
+    }, {
+      name: "aegaerg",
+      price: 152,
+      left: 10,
+      discount: 101
+    }, {
+      name: "asfawer",
+      price: 412,
+      left: 103,
+      discount: 1601
+    }]
+  constructor() {
+  }
 }
