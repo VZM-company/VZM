fbf09221faba7d289b50170ff17ca0946d3cc332
EvgeniyVashinko <e.vashinko@gmail.com>
2021-05-08 23:41:38 +0300

    add DataManager

---
+++ VZM.Data/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using VZM.Interfaces;
+
+namespace VZM.Data
+{
+    public class DataManager
+    {
+        public IProductRepository Products { get; set; }
+        public IUserRepository Users { get; set; }
+
+        public DataManager(IProductRepository productRepository, IUserRepository userRepository)
+        {
+            Products = productRepository;
+            Users = userRepository;
+        }
+    }
+}
