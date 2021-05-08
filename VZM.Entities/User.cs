ba66821765f959ff9355ba97732db5f587419430
Tourie <makar.pundis@yandex.ru>
2021-05-08 23:20:05 +0300

    add username

--- VZM.Entities/User.cs
+++ VZM.Entities/User.cs
@@ -7,6 +7,7 @@ namespace VZM.Entities
     {
         public Guid UserId { get; set; }
         public string Name { get; set; }
+        public string UserName { get; set; }
         public string PasswordHash { get; set; }
         public string Email { get; set; }
         public DateTime CreatedAt { get; set; }
