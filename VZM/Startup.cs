4ca8222c41d75c5b7a7931f7cf58895cf99ebb4d
Tourie <makar.pundis@yandex.ru>
2021-05-08 23:40:13 +0300

    changed startup

--- VZM/Startup.cs
+++ VZM/Startup.cs
@@ -33,6 +33,8 @@ namespace VZM
             services.AddSingleton(new SqlConnection(Configuration["DefaultConnection"]));
 
             services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<DataManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
