36deda699ae0dc2cfa20d4971a17b2c6910be9e0
EvgeniyVashinko <e.vashinko@gmail.com>
2021-05-17 18:05:00 +0300

    add triggers to db

---
+++ Db/Tables/Journal.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Journal]
+(
+ [JournalId] uniqueidentifier NOT NULL,
+ [Type] varchar(50) NOT NULL,
+ [Description] varchar(50) NOT NULL,
+ [CreatedAt] datetime NOT NULL,
+);
+GO
---
+++ Db/Tables/TrProduct.sql
@@ -0,0 +1,17 @@
+﻿CREATE TRIGGER TR_Audit_Products ON [dbo].[Product]
+    FOR INSERT, UPDATE, DELETE
+AS
+BEGIN
+        INSERT INTO [dbo].[Journal] (
+            JournalId,
+            Description ,
+            Type ,
+            CreatedAt
+        ) VALUES (
+            NEWID(),
+            'Product was changed',
+            'Update',
+            GETDATE()
+        )
+END
+GO
